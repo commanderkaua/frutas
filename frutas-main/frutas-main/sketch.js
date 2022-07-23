482d10d463e32259380e704fdff9db3a08ec7a34
commanderkaua <103076194+commanderkaua@users.noreply.github.com>
2022-07-23 13:05:43 -0300

    Add files via upload

---
+++ frutas-main/frutas-main/sketch.js
@@ -0,0 +1,154 @@
+//Estados do jogo
+var PLAY=1;
+var END=0;
+var gameState=1;
+var aleatório;
+var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit,gameOverSound,knifeSound, position;
+var knifeImage , fruit1, fruit2 ,fruit3,fruit4,monsterAnimation,  gameOverImage;
+estado = PLAY;
+var gameOver;
+
+function preload(){
+  
+  knifeImage = loadImage("knife.png");
+  monsterAnimation = loadAnimation("alien1.png","alien2.png")
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  gameOverImage = loadImage("gameover.png");
+  gameOverSound = loadSound("gameover.mp3");
+  knifeSound = loadSound("knifeSwoosh.mp3");
+}
+
+
+
+function setup() {
+  createCanvas(600, 600);
+  
+  //criar espada
+   knife=createSprite(40,200,20,20);
+   knife.addImage(knifeImage);
+   knife.scale=0.7
+  
+  //definir colisor para espada
+  knife.setCollider("rectangle",0,0,40,40);
+
+  // Variáveis de pontuação e grupos
+  score=0;
+  fruitGroup= new Group();
+  monsterGroup= new Group();
+  
+}
+
+function draw() {
+  background("lightblue");
+  
+  if(gameState===PLAY){
+    
+    //Chamar função de frutas e monstros
+    fruits();
+    Monster();
+    
+    // Mova a espada com o mouse
+    knife.y=World.mouseY;
+    knife.x=World.mouseX;
+  
+    // Aumenta a pontuação se a espada tocar na fruta
+    if(fruitGroup.isTouching(knife)){
+      fruitGroup.destroyEach();
+      score = score+1
+    }
+    else
+     {
+      
+      // Vá para o estado final se a espada tocar o inimigo
+      if(monsterGroup.isTouching(knife)){
+        gameState=END;
+        
+        //adicione o som do gameover (fim de jogo) aqui
+        gameOverSound.play();
+        fruitGroup.destroyEach();
+        monsterGroup.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        monsterGroup.setVelocityXEach(0);
+       
+        
+        
+        if(gameState==END){
+        knife.addImage(gameOverImage);
+        knife.scale=2;
+        knife.x=300;
+        knife.y=300;
+        }
+      }
+    }
+  }
+  
+  drawSprites();
+  //Exibir pontuação
+  textSize(25);
+  text("Score : "+ score,250,50);
+}
+
+
+function Monster(){
+  if(World.frameCount%200===0){
+    monster=createSprite(400,200,20,20);
+    monster.addAnimation("moving", monsterAnimation);
+    monster.y=Math.round(random(100,550));
+    //atualize o código abaixo para aumentar a velocidade do monsterGroup (grupo de monstros) em 10
+    monster.velocityX = -8;
+    monster.setLifetime=50;
+    
+    monsterGroup.add(monster);
+    monsterGroup.setVelocityXEach-(10+score/50);
+  }
+}
+
+function fruits(){
+  if(World.frameCount%80===0){
+    position = Math.round(random(1,2));
+    fruit=createSprite(400,200,20,20);
+    fruitGroup.setVelocityEach(4);
+     //usar uma variável aleatória para mudar a posição da fruta e tornar mais desafiador
+    fruit = createSprite(aleatório,200,20,20);
+
+    if(position==1)
+    {      
+    fruit.x=600;
+    //atualize o código abaixo para aumentar a velocidade do fruitGroup (grupo de frutas) em 4
+    fruitGroup.setVelocityXEach = -(4*score/20);
+    }
+    else
+    {
+      if(position==2){
+      fruit.x=0;
+      
+     //atualize o código abaixo para aumentar a velocidade do fruitGroup (grupo de frutas) em 4
+     fruitGroup.velocityX = -(4+score/50);
+      }
+    }
+    
+    fruit.scale=0.2;
+     
+     fruit.debug = false;
+     r=Math.round(random(1,4));
+    if (r == 1) {
+      fruit.addImage(fruit1);
+    } else if (r == 2) {
+      fruit.addImage(fruit2);
+    } else if (r == 3) {
+      fruit.addImage(fruit3);
+    } else {
+      fruit.addImage(fruit4);
+    }
+    
+    fruit.y=Math.round(random(50,550));
+   
+    
+    fruit.setLifetime=100;
+    
+    fruitGroup.add(fruit);
+  }
+}
